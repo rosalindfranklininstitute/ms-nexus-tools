db8ed253282f18773981c7f9bc3185af23fd0029
McDougall <Duncan.McDougall@rfi.ac.uk>
2026-05-19 13:18:17 +0100

    Vectorise implimentation of p2 percentiles

--- tests/test_percentiles.py
+++ tests/test_percentiles.py
@@ -21,44 +21,63 @@ def f2(array):
 
 
 class P2Histogram:
-    def __init__(self, b: int):
+    def __init__(self, b: int, shape: nxlib.bounds.Shape = (1,)):
         self.b = b
-        self.heights = np.zeros((b + 1))
-        self.positions = np.zeros((b + 1))
+        self.shape = shape
+        self.selectors = (slice(None),) * len(shape)
+        self.heights = np.zeros((*shape, b + 1))
+        self.positions = np.zeros((*shape, b + 1))
 
         self.ii = 0
 
     def add(self, x):
-        if self.ii <= self.b:
-            self.heights[self.ii] = x
-            self.positions[self.ii] = self.ii
-            if self.ii == self.b:
-                sort_inx = np.argsort(self.heights)
-                heights = np.array([self.heights[inx] for inx in sort_inx])
+
+        if np.isscalar(x):
+            assert self.shape == (1,)
+            x = np.array([x])
+        elif isinstance(x, np.ndarray):
+            assert x.shape == self.shape
         else:
-            if x < self.heights[0]:
-                self.heights[0] = x
-                k = 0
-            elif self.heights[-1] < x:
-                self.heights[-1] = x
-                k = self.b - 1
-            else:
-                k = bisect.bisect_left(self.heights, x) - 1
+            raise NotImplementedError(
+                "Input value must be either a scalar or and array"
+            )
 
-            self.positions[k + 1 :] += 1
+        if self.ii <= self.b:
+            self.heights[*self.selectors, self.ii] = x
+            self.positions[*self.selectors, self.ii] = self.ii
+            if self.ii == self.b:
+                sort_inx = np.argsort(self.heights, axis=-1)
+                self.heights = np.take_along_axis(self.heights, sort_inx, axis=-1)
+        else:
+            padding_shape = [(0, 0) for _ in range(len(self.shape))]
+            padding_shape.append((1, 1))
 
-            desired_positions = np.arange(self.b + 1) * self.ii / self.b
-            d = (desired_positions - self.positions)[1:-1]
+            min_value = x < self.heights[*self.selectors, 0]
+            self.heights[min_value, 0] = x[min_value]
 
-            n_minus = np.diff(self.positions)[:-1]
-            n_plus = np.diff(self.positions)[1:]
+            max_value = x > self.heights[*self.selectors, -1]
+            self.heights[max_value, -1] = x[max_value]
+
+            for k in range(self.b, 0, -1):
+                mask = x < self.heights[*self.selectors, k]
+                self.positions[mask, k] += 1
+
+            self.positions[*self.selectors, -1] = self.ii
+
+            desired_positions = np.tile(
+                np.arange(self.b + 1) * self.ii / self.b, self.shape
+            ).reshape((*self.shape, self.b + 1))
+            d = (desired_positions - self.positions)[*self.selectors, 1:-1]
+
+            n_minus = np.diff(self.positions, axis=-1)[*self.selectors, :-1]
+            n_plus = np.diff(self.positions, axis=-1)[*self.selectors, 1:]
 
             d_mask = ((n_plus > 1) * (d >= 1)) + ((n_minus > 1) * (d <= -1))
 
             if np.any(d_mask):
-                q = self.heights[1:-1]
-                q_minus = np.diff(self.heights)[:-1]
-                q_plus = np.diff(self.heights)[1:]
+                q = self.heights[*self.selectors, 1:-1]
+                q_minus = np.diff(self.heights, axis=-1)[*self.selectors, :-1]
+                q_plus = np.diff(self.heights, axis=-1)[*self.selectors, 1:]
 
                 d[d < 0] = -1
                 d[d > 0] = 1
@@ -74,9 +93,16 @@ class P2Histogram:
                 )
 
                 q_mask = (
-                    (quad_q > self.heights[:-2]) * (self.heights[2:] > quad_q) * d_mask
+                    (quad_q > self.heights[*self.selectors, :-2])
+                    * (self.heights[*self.selectors, 2:] > quad_q)
+                    * d_mask
                 )
-                self.heights[[False, *q_mask, False]] = quad_q[q_mask]
+                padded_q_mask = np.pad(
+                    q_mask,
+                    pad_width=padding_shape,
+                    constant_values=np.full((len(padding_shape), 2), False),
+                )
+                self.heights[padded_q_mask] = quad_q[q_mask]
 
                 l_mask = (q_mask == False) * d_mask
                 if np.any(l_mask):
@@ -89,8 +115,19 @@ class P2Histogram:
                         n_minus[l_mask],
                         n_plus[l_mask],
                     )
-                    self.heights[[False, *l_mask, False]] = lin_q[l_mask]
-                self.positions[[False, *d_mask, False]] += d[d_mask]
+                    padded_l_mask = np.pad(
+                        l_mask,
+                        pad_width=padding_shape,
+                        constant_values=np.full((len(padding_shape), 2), False),
+                    )
+                    self.heights[padded_l_mask] = lin_q[l_mask]
+
+                padded_d_mask = np.pad(
+                    d_mask,
+                    pad_width=padding_shape,
+                    constant_values=np.full((len(padding_shape), 2), False),
+                )
+                self.positions[padded_d_mask] += d[d_mask]
         self.ii += 1
 
     @staticmethod
@@ -106,7 +143,7 @@ class P2Histogram:
         positive = d > 0
         result = np.zeros(q.shape)
         result[negative] = q[negative] - (q_minus[negative]) / (n_minus[negative])
-        result[positive] = q[negative] + (q_plus[negative]) / (n_plus[negative])
+        result[positive] = q[positive] + (q_plus[positive]) / (n_plus[positive])
         result[neutral] = q[neutral]
         return result
 
@@ -139,15 +176,14 @@ def test_percentiles():
         ]
     )
     percentiles = np.percentile(data, [0, 25, 50, 75, 100])
-    marker_heights = np.array([0.02, 0.5, 4.44, 17.22, 38.62])
 
     p2_perc = P2Histogram(b=4)
 
-    for x in data:
+    for ii, x in enumerate(data):
         p2_perc.add(x)
 
     np.testing.assert_allclose(
-        p2_perc.heights, np.array([0.02, 0.506, 3.370, 14.61, 38.62]), rtol=1e-3
+        p2_perc.heights[0, :], np.array([0.02, 0.5232, 4.402, 11.29, 38.62]), rtol=1e-3
     )
 
     # Extra 100 random data points
@@ -159,9 +195,9 @@ def test_percentiles():
 
     error = 1 - np.dot(
         percentiles / np.linalg.norm(percentiles),
-        p2_perc.heights / np.linalg.norm(p2_perc.heights),
+        (p2_perc.heights / np.linalg.norm(p2_perc.heights))[0, :],
     )
-    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights, 2)))
+    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights[0, :], 2)))
     assert r2 < 1.7
     assert error < 1e-2
 
@@ -174,8 +210,8 @@ def test_percentiles():
 
     error = 1 - np.dot(
         percentiles / np.linalg.norm(percentiles),
-        p2_perc.heights / np.linalg.norm(p2_perc.heights),
+        (p2_perc.heights / np.linalg.norm(p2_perc.heights))[0, :],
     )
-    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights, 2)))
+    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights[0, :], 2)))
     assert r2 < 0.17
     assert error < 1e-4
