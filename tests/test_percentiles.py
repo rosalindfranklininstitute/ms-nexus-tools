03756076f4eb5bb2036369489dab7d1020bb43a0
McDougall <Duncan.McDougall@rfi.ac.uk>
2026-05-19 13:39:38 +0100

    Adds test for 2D P2 Percentiles

--- tests/test_percentiles.py
+++ tests/test_percentiles.py
@@ -33,10 +33,16 @@ class P2Histogram:
     def add(self, x):
 
         if np.isscalar(x):
-            assert self.shape == (1,)
+            if self.shape != (1,):
+                raise ValueError(
+                    f"Provided a scalar, but expected data with shape {self.shape}"
+                )
             x = np.array([x])
         elif isinstance(x, np.ndarray):
-            assert x.shape == self.shape
+            if x.shape != self.shape:
+                raise ValueError(
+                    f"Provided data with shape {x.shape}, but expected a shape of {self.shape}"
+                )
         else:
             raise NotImplementedError(
                 "Input value must be either a scalar or and array"
@@ -175,6 +181,7 @@ def test_percentiles():
             11.37,
         ]
     )
+    expected_results = np.array([0.02, 0.5232, 4.402, 11.29, 38.62])
     percentiles = np.percentile(data, [0, 25, 50, 75, 100])
 
     p2_perc = P2Histogram(b=4)
@@ -182,9 +189,7 @@ def test_percentiles():
     for ii, x in enumerate(data):
         p2_perc.add(x)
 
-    np.testing.assert_allclose(
-        p2_perc.heights[0, :], np.array([0.02, 0.5232, 4.402, 11.29, 38.62]), rtol=1e-3
-    )
+    np.testing.assert_allclose(p2_perc.heights[0, :], expected_results, rtol=1e-3)
 
     # Extra 100 random data points
     new_data = np.random.random((100)) * 40
@@ -213,5 +218,75 @@ def test_percentiles():
         (p2_perc.heights / np.linalg.norm(p2_perc.heights))[0, :],
     )
     r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights[0, :], 2)))
-    assert r2 < 0.17
+    assert r2 < 0.18
     assert error < 1e-4
+
+
+def test_percentiles_2d():
+
+    # Data from the paper
+    data = np.array(
+        [
+            0.02,
+            0.15,
+            0.74,
+            3.39,
+            0.83,
+            22.37,
+            10.15,
+            15.43,
+            38.62,
+            15.92,
+            34.60,
+            10.28,
+            1.47,
+            0.40,
+            0.05,
+            11.39,
+            0.27,
+            0.42,
+            0.09,
+            11.37,
+        ]
+    )
+    expected_results = np.array([0.02, 0.5232, 4.402, 11.29, 38.62])
+    data = np.tile(data, (2, 1)).T
+    expected_results = np.tile(expected_results, (2, 1))
+    ic(data.shape)
+    ic(expected_results.shape)
+    percentiles = np.percentile(data, [0, 25, 50, 75, 100], axis=0).T
+    ic(percentiles.shape)
+    ic(percentiles)
+
+    p2_perc = P2Histogram(
+        b=4,
+        shape=(2,),
+    )
+
+    for ii, x in enumerate(data):
+        p2_perc.add(x)
+
+    np.testing.assert_allclose(p2_perc.heights, expected_results, rtol=1e-3)
+
+    # Extra 100 random data points
+    new_data = np.random.random((100, 2)) * 40
+    for x in new_data:
+        p2_perc.add(x)
+    data = np.concat([data, new_data])
+    percentiles = np.percentile(data, [0, 25, 50, 75, 100], axis=0).T
+
+    ic(percentiles)
+    ic(p2_perc.heights)
+
+    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights, 2)))
+    assert r2 < 1.7
+
+    # Extra 1000 random data points
+    new_data = np.random.random((1000, 2)) * 40
+    for x in new_data:
+        p2_perc.add(x)
+    data = np.concat([data, new_data])
+    percentiles = np.percentile(data, [0, 25, 50, 75, 100], axis=0).T
+
+    r2 = np.sqrt(np.average(np.pow(percentiles - p2_perc.heights, 2)))
+    assert r2 < 0.18
